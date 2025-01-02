626ff94b03e7d2f2eeb953dbaae0a9ff11d04a60
Kristian <kristian.dam.pedersen@gmail.com>
2025-01-02 14:07:36 +0100

    Adds interface function for requesting a vm

--- apps/browservm/lib/browservm/vms.ex
+++ apps/browservm/lib/browservm/vms.ex
@@ -2,6 +2,8 @@ defmodule Browservm.Vms do
   use Ash.Domain
 
   resources do
-    resource Browservm.Vms.Vm
+    resource Browservm.Vms.Vm do
+      define :request_new_vm, args: [:machine_name, :ami_id], action: :request_new_vm
+    end
   end
 end
